13842efb8e3773c55b432b8dd45122cae01c4b9b
James Zhang <james.yi.zhang@gmail.com>
2023-07-16 00:22:46 -0600

    parameter optimization results

---
+++ alphaparameteroptimization.py
@@ -0,0 +1,42 @@
+"""
+    param_reset  param_npairs  param_enter  param_exit  param_stop  \
+0            90            10          2.0         1.0         3.5   
+1            90            10          2.0         1.5         3.5   
+2            90            10          2.0         2.0         3.5   
+3            90            10          2.5         1.0         3.5   
+4            90            10          2.5         1.5         3.5   
+5            90            10          2.5         2.0         3.5   
+6            90            10          3.0         1.0         3.5   
+7            90            10          3.0         1.5         3.5   
+8            90            10          3.0         2.0         3.5   
+9            90            25          2.0         1.0         3.5   
+10           90            25          2.0         1.5         3.5   
+11           90            25          2.0         2.0         3.5   
+12           90            25          2.5         1.0         3.5   
+13           90            25          2.5         1.5         3.5   
+14           90            25          2.5         2.0         3.5   
+15           90            25          3.0         1.0         3.5   
+16           90            25          3.0         1.5         3.5   
+17           90            25          3.0         2.0         3.5   
+
+                                               params     score  
+0   {'reset': 90, 'npairs': 10, 'enter': 2, 'exit'...  0.625252  
+1   {'reset': 90, 'npairs': 10, 'enter': 2, 'exit'...  0.599652  
+2   {'reset': 90, 'npairs': 10, 'enter': 2, 'exit'...  1.844196  
+3   {'reset': 90, 'npairs': 10, 'enter': 2.5, 'exi...  1.130319  
+4   {'reset': 90, 'npairs': 10, 'enter': 2.5, 'exi...  1.166980  
+5   {'reset': 90, 'npairs': 10, 'enter': 2.5, 'exi...  1.573566  
+6   {'reset': 90, 'npairs': 10, 'enter': 3, 'exit'...  0.494403  
+7   {'reset': 90, 'npairs': 10, 'enter': 3, 'exit'...  0.448972  
+8   {'reset': 90, 'npairs': 10, 'enter': 3, 'exit'...  0.459191  
+9   {'reset': 90, 'npairs': 25, 'enter': 2, 'exit'...  0.672127  
+10  {'reset': 90, 'npairs': 25, 'enter': 2, 'exit'...  1.432157  
+11  {'reset': 90, 'npairs': 25, 'enter': 2, 'exit'...  2.159327  
+12  {'reset': 90, 'npairs': 25, 'enter': 2.5, 'exi...  0.917044  
+13  {'reset': 90, 'npairs': 25, 'enter': 2.5, 'exi...  1.318499  
+14  {'reset': 90, 'npairs': 25, 'enter': 2.5, 'exi...  1.924138  
+15  {'reset': 90, 'npairs': 25, 'enter': 3, 'exit'...  0.477280  
+16  {'reset': 90, 'npairs': 25, 'enter': 3, 'exit'...  0.854372  
+17  {'reset': 90, 'npairs': 25, 'enter': 3, 'exit'...  1.050643  
+
+"""
